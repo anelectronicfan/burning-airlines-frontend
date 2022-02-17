0eb900b4f0ed09974fc2c91d5b153d2fb6f242db
Angela Zhang <a_zhang93@hotmail.com>
2022-02-17 12:48:55 +1100

    added deleteflight function

--- src/components/FlightsIndex.js
+++ src/components/FlightsIndex.js
@@ -4,6 +4,7 @@ import './FlightsIndex.css';
 
 
 const RAILS_FLIGHTS_SHOW_BASE_URL = "http://localhost:3000/api/admin/flights/"
+const RAILS_FLIGHTS_DELETE_BASE_URL = "http://localhost:3000/api/flights/"
 
 export default class FlightsIndex extends Component {
 
@@ -14,27 +15,46 @@ export default class FlightsIndex extends Component {
   }
 
   componentDidMount() {
-        console.log("Welcome Flights Index: ");
-        this.getFlightsIndex();
-    }
+    console.log("Welcome Flights Index: ");
+    this.getFlightsIndex();
+    window.setInterval(this.getFlightsIndex,30000);
+  }
     
 
-    getFlightsIndex = async () => {
-        try{
-            const res = await axios.get( RAILS_FLIGHTS_SHOW_BASE_URL);
-            console.log("Flights Index Response: ",res.data);
-            this.setState({flights: res.data, loading:false })
-        }catch(err){
-            console.log('ERROR loading AJAX flights index: ',err);
-        }
-    }//get Reservations
+  getFlightsIndex = async () => {
+    try{
+        const res = await axios.get( RAILS_FLIGHTS_SHOW_BASE_URL);
+        console.log("Flights Index Response: ",res.data);
+        this.setState({flights: res.data, loading:false })
+    }catch(err){
+        console.log('ERROR loading AJAX flights index: ',err);
+    }
+  }//getFlightIndex
+
+  deleteFlight = async () => {
+    try{
+      const res = await axios.delete(RAILS_FLIGHTS_DELETE_BASE_URL, {flights: res.data.id })
+
+
+      this.setState({
+        flights: [ res.data, ...this.state.flights ]
+      })
+    }catch(err){
+      console.log( 'Error deleting flights: ', err)
+    }
+  };
 
   render() {
     const { loading, flights } = this.state;
    
     return (
+
       <div>
         <h1>FlightsIndex</h1>
+        <div className="flightIndex">
+          <p>Add New Flight</p>
+        </div>
+
         {
           loading
           ?
