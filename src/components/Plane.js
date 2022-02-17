7902555d8ce572daae8d8928e962caf09c90dc08
danyip <danielyip90@gmail.com>
2022-02-17 11:43:54 +1100

    working on plane.rb

--- src/components/Plane.js
+++ src/components/Plane.js
@@ -1,9 +1,48 @@
+// import React, { Component } from 'react'
+
+// export default class Plane extends Component {
+  
+  
+//   render() {
+//     return (
+//       <div className="plane-seating-container"> 
+
+
+
+
+ 
+
 import React, { Component } from 'react'
 
 export default class Plane extends Component {
+  
+  
+  
   render() {
     return (
-      <div>Plane</div>
+      <div className="plane-seating-container"> 
+        {
+          Array(this.props.plane.total_rows).map(row => {
+
+            return <div></div>
+
+
+          })
+          
+        }
+
+
+      </div>
     )
   }
+
+
+
+
+
+
 }
+
+
+
+  
