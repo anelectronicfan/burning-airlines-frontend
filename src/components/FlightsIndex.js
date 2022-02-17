8b4fdfde5215181a60edb43815fe6b924682ac95
Angela Zhang <a_zhang93@hotmail.com>
2022-02-17 11:45:24 +1100

    added flights: date, id, to>from

--- src/components/FlightsIndex.js
+++ src/components/FlightsIndex.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import './FlightsIndex.css';
 
 
-const RAILS_FLIGHTS_SHOW_BASE_URL = "http://localhost:3000/admin/flights/"
+const RAILS_FLIGHTS_SHOW_BASE_URL = "http://localhost:3000/api/admin/flights/"
 
 export default class FlightsIndex extends Component {
+
+  state = {
+    flights: [],
+  
+  }
 
   componentDidMount() {
         console.log("Welcome Flights Index: ");
@@ -17,16 +23,47 @@ export default class FlightsIndex extends Component {
         try{
             const res = await axios.get( RAILS_FLIGHTS_SHOW_BASE_URL);
             console.log("Flights Index Response: ",res.data);
+            this.setState({flights: res.data })
         }catch(err){
             console.log('ERROR loading AJAX flights index: ',err);
         }
     }//get Reservations
 
   render() {
+    const { flights } = this.state;
+
     return (
       <div>
         <h1>FlightsIndex</h1>
 
+        <div className="flightIndex">
+          <p>All Flights</p>
+          <ul>
+            <p className="sectionTitle">Date</p>
+            {
+              flights.map( f => <li key={f.id}>{f.date}</li>)
+            }
+          </ul>
+          <ul>
+            <p className="sectionTitle">Flight ID</p>
+            {
+              flights.map( f => <li key={f.id}>{f.airplane_id}</li>)
+            }
+          </ul>
+          <ul>
+            <p className="sectionTitle">To > From</p>
+            {
+              flights.map( f => <li key={f.id}>{f.origin} > {f.destination}</li>)
+            }
+          </ul>
+          <ul>
+            <p className="sectionTitle">Seats</p>
+            {
+              flights.map( f => <li key={f.id}>{f.total_rows} > {f.total_columns}</li>)
+            }
+          </ul>
+        </div>
+      
 
 
       </div>
